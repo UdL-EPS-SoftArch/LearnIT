0a3f66b553f8ff7211b534d056ee0cb915406881
etahirovic1 <etahirovic1@etf.unsa.ba>
2020-05-04 23:40:37 +0200

    Additional changes to layout.

--- src/app/level/level-list/level-list.component.ts
+++ src/app/level/level-list/level-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Level} from '../level';
 import {LevelService} from "../level.service";
 import {Router} from "@angular/router";
-import {Sort} from "@lagoshny/ngx-hal-client";
 
 @Component({
   selector: 'app-level-list',
@@ -13,20 +12,20 @@ export class LevelListComponent implements OnInit {
 
   public levels: Level[] = [];
   public totalRecipes = 0;
-  private sorting: Sort[] = [{ path: 'name', order: 'ASC' }];
 
   constructor(public router: Router, private levelService: LevelService) {
   }
 
   ngOnInit() {
-    this.levelService.getAll({sort: this.sorting}).subscribe(
+    this.levelService.getAll().subscribe(
       (levels: Level[]) => {
         this.levels = levels;
         this.totalRecipes = this.levelService.totalElement();
         console.log(this.levels)
       });
-
   }
+
+
 }
 
 
