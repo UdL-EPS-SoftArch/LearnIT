5c7e52d8cf0f10a986ece2b7cd88c903e40ffe82
jsolanes14 <jordisolanes14@gmail.com>
2020-05-08 00:30:05 +0200

    FinalLevelExam service added

---
+++ src/app/FinalLevelExam/FinalLevelExam.service.ts
@@ -0,0 +1,19 @@
+import {Injectable, Injector} from '@angular/core';
+import {RestService} from '@lagoshny/ngx-hal-client';
+import {FinalLevelExam} from './FinalLevelexam';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class FinalLevelExamService extends RestService<FinalLevelExam>{
+
+  constructor(injector: Injector) {
+    super(FinalLevelExam, 'FinalLevelExam', injector);
+  }
+
+  public findById(id: number): Observable<FinalLevelExam[]> {
+    const options: any = {params: [{key: 'id', value: id}]};
+    return this.search('findById', options);
+  }
+}
+
+
